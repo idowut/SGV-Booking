cbbbca95c2d00de49158cc6a97283cef133831b7
Trong Le <105897244+TinisTin@users.noreply.github.com>
2023-09-06 11:30:22 +0930

    Basic Guest Details

---
+++ Controllers/BookingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGV_Booking.Models;
+
+namespace SGV_Booking.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Booking()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,8 @@ namespace SGV_Booking.Controllers
             return View();
         }
 
+       
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
