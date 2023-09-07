bde7a4ac4650e12b596ce03e424135ef5b41103f
Anthony Bui <105613060+buiah001@users.noreply.github.com>
2023-09-07 14:27:27 +0930

    PROJ-18, Completed

---
+++ Controllers/RestaurantsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SGV_Booking.Data;
+using SGV_Booking.Models;
+
+namespace SGV_Booking.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        private readonly SGVDatabaseContext _context;
+
+        public RestaurantsController(SGVDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Restaurants
+        public async Task<IActionResult> Index()
+        {
+            var sGVDatabaseContext = _context.Restaurants.Include(r => r.RestaurantAddress);
+            return View(await sGVDatabaseContext.ToListAsync());
+        }
+
+        // GET: Restaurants/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Restaurants == null)
+            {
+                return NotFound();
+            }
+
+            var restaurant = await _context.Restaurants
+                .Include(r => r.RestaurantAddress)
+                .FirstOrDefaultAsync(m => m.RestaurantId == id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return View(restaurant);
+        }
+
+        // GET: Restaurants/Create
+        public IActionResult Create()
+        {
+            ViewData["RestaurantAddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId");
+            return View();
+        }
+
+        // POST: Restaurants/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RestaurantId,RestaurantName,RestaurantAddressId,RestaurantPhoneNumber")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(restaurant);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RestaurantAddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", restaurant.RestaurantAddressId);
+            return View(restaurant);
+        }
+
+        // GET: Restaurants/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Restaurants == null)
+            {
+                return NotFound();
+            }
+
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+            ViewData["RestaurantAddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", restaurant.RestaurantAddressId);
+            return View(restaurant);
+        }
+
+        // POST: Restaurants/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RestaurantId,RestaurantName,RestaurantAddressId,RestaurantPhoneNumber")] Restaurant restaurant)
+        {
+            if (id != restaurant.RestaurantId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(restaurant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RestaurantExists(restaurant.RestaurantId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RestaurantAddressId"] = new SelectList(_context.Addresses, "AddressId", "AddressId", restaurant.RestaurantAddressId);
+            return View(restaurant);
+        }
+
+        // GET: Restaurants/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Restaurants == null)
+            {
+                return NotFound();
+            }
+
+            var restaurant = await _context.Restaurants
+                .Include(r => r.RestaurantAddress)
+                .FirstOrDefaultAsync(m => m.RestaurantId == id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return View(restaurant);
+        }
+
+        // POST: Restaurants/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Restaurants == null)
+            {
+                return Problem("Entity set 'SGVDatabaseContext.Restaurants'  is null.");
+            }
+            var restaurant = await _context.Restaurants.FindAsync(id);
+            if (restaurant != null)
+            {
+                _context.Restaurants.Remove(restaurant);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RestaurantExists(int id)
+        {
+          return (_context.Restaurants?.Any(e => e.RestaurantId == id)).GetValueOrDefault();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SGV_Booking.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
+
+//SGVDatabase Conneciton
+builder.Services.AddDbContext<SGVDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SGVContext") ??
+    throw new InvalidOperationException("Conneciton string 'SGVDatabase' is not found.")));
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
